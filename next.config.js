3e2897661910a436de753959c8f509262cdcc4d3
jfertaj <jfertaj@gmail.com>
2026-01-07 17:16:05 +0100

    fix: expose ADMIN_TOKEN in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 const nextConfig = {
   output: 'standalone',   // 👈 necesario para SSR en Amplify Hosting
   reactStrictMode: true,
+  env: {
+    // Pasar explícitamente la variable al runtime de Next.js
+    ADMIN_TOKEN: process.env.ADMIN_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
